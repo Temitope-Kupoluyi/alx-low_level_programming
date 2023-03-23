1c9b5ef28369b7cdff1a93b7bd7680f09cb12470
Temitope-Kupoluyi <Kupoluyitemitope2004@gmail.com>
2023-03-23 15:45:37 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - This functiom prints the alphabet in lower case
+ * print_alphabet - This function prints alphabet in lower case
  *
  * Return: 0
  */
-
-void print_alphabet(void);
-{
-char i;
+void print_alphabet(void)
+{char i;
 for (i = 'a' ; i <= 'z' ; i++)
 _putchar(i);
 _putchar('\n');
