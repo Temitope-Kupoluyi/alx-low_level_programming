540d7c06f9afa42c36c5d42919ddfb92556fbc97
Temitope-Kupoluyi <Kupoluyitemitope2004@gmail.com>
2023-03-24 10:13:32 -0700

    print last digit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main - check the code
  *
@@ -8,12 +9,12 @@
 
 int main(void)
 {
-	    int r;
+	int r;
 
-	        print_last_digit(98);
-		    print_last_digit(0);
-		        r = print_last_digit(-1024);
-			    _putchar('0' + r);
-			        _putchar('\n');
-				    return (0);
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * print_last_digit - function that prints the last digit of a number
+ * print_last_digit - function that prints the last digit of a mumber
  *
  * @a: character to be checked
  *
- * Return: 1
+ * Return: b
  */
 
 int print_last_digit(int a)
 {
 	int b;
-
 	b = a % 10;
 	if (a < 0)
 		b = -b;
 	_putchar(b + '0');
-	return (0);
+	return (b);
 }
